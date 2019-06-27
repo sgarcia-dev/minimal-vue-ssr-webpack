750fbb58fa8188f168b5f378a764bc00187f0d07
Sergei Garcia Ortega <sergei.garcia@digitalonus.com>
2019-06-27 18:58:36 -0500

    chore: Cleanup code

--- router.js
+++ router.js
@@ -8,11 +8,7 @@ const router = express.Router();
 const renderer = createRenderer();
 
 router.get('*', (req, res) => {
-  debugger;
   const context = { url: req.url };
-  // renderer
-  //   .renderToStream(context)
-  //   .pipe(res);
   renderer.renderToString(context, (err, html) => {
     if (err) {
       console.error(err);
@@ -23,11 +19,9 @@ router.get('*', (req, res) => {
 });
 
 function createRenderer() {
-  debugger;
-  const bundle = resolve(__dirname, './dist/server.bundle.js')
-  // const bundle = require('./dist/vue-ssr-bundle.json');
+  const bundlePath = resolve(__dirname, './dist/server.bundle.js');
   const template = fs.readFileSync('./index.html', 'utf-8');
-  return createBundleRenderer(bundle, { template });
+  return createBundleRenderer(bundlePath, { template });
 }
 
 module.exports = router;
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.config');
-const VueSSRPlugin = require('vue-ssr-webpack-plugin');
 
 module.exports = merge(baseWebpackConfig, {
   target: 'node',
@@ -16,7 +15,6 @@ module.exports = merge(baseWebpackConfig, {
   devtool: 'source-map',
   externals: Object.keys(require('./package.json').dependencies),
   plugins: [
-    // new VueSSRPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
